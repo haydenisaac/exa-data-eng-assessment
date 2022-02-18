02b53fe998bc12fbaf7e2751cc240a7f1d66fc1f
Hayden Isaac <haydenisaac2906@gmail.com>
2022-02-18 13:36:28 +0000

    Changed databse to mongo as it's easier to work with json data

--- app/connection_test.py
+++ app/connection_test.py
@@ -1,6 +1,7 @@
-from sqlalchemy import create_engine
-mysql_conn_str = "mysql+pymysql://root:root@mysql_db:3306/patients"
-engine = create_engine(mysql_conn_str)
-connection = engine.connect()
-q = connection.execute('SHOW DATABASES')
-print(q.fetchall())
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://root:password@mongo:27017")
+
+db = client["test"]
+collection = db["author"]
+collection.insert_one({"name": "Hayden"})
